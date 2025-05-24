f4c1ba73b73ace84b7b9447ddc5d50000cb6fe22
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-24 23:49:46 +0100

    First commit

---
+++ nestedObject.js
@@ -0,0 +1,12 @@
+const person = {
+    name:'Mike',
+    age:10,
+    status:'not found',
+    friends:['Joe','Farjana','Atikur'],
+    details:{
+        job:"yes",
+        isMarried:true,
+        mothersName:"Hasina"
+    }
+}
+console.log(person["details"]["fathersName"]);

8c4c4c195eab585300002207144334b2218e0cf6
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-26 10:06:00 +0100

    Encapsulation

--- Encapsulation.js
+++ Encapsulation.js
@@ -1,21 +1,27 @@
-const person = {
-  name: "Mike",
-  action: function () {
-    console.log(this.name);
-  },
-  sleep(){
-    console.log(`${this.name} sleep at 10 pm`);
-  }
-};
-person.action();
-
-console.log(person.name);
-person.sleep();
-class humanBeing {
-  constructor(name, age) {
-    this.name = name;
+class person  {
+    #status
+  constructor(name,age,status){
+    this.name=name;
     this.age = age;
+    this.status= status;
   }
-}
-let Joe = new humanBeing("MIKE",35);
-console.log(Joe);
+//   action: function () {
+//     console.log(this.name);
+//   },
+//   sleep(){
+//     console.log(`${this.name} sleep at 10 pm`);
+  
+};
+let jack = new person("Joe",45,"married");
+console.log(jack);
+
+// console.log(person.name);
+// person.sleep();
+// class humanBeing {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+// let Joe = new humanBeing("MIKE",35);
+// console.log(Joe);
